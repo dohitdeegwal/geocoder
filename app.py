ca5b6ba790baf97d0f356cd8793abd271a396bb7
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2022-02-28 21:53:07 +0530

    ,

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ from geopy.geocoders import ArcGIS
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/success", methods=["POST", "GET"])
 def success():
     global file, df
     if request.method == "POST":
-        file=request.files["file"]
+        file = request.files["file"]
         try:
             df = pd.read_csv(file)
             address_col_name = "not found"
@@ -23,45 +25,43 @@ def success():
                 if case in columns:
                     address_col_name = case
                     break
-            
+
             if address_col_name == "not found":
                 return render_template("index.html", text="File does not contain an address column", classType="text-warning")
-                
-            
+
             addresses = list(df[address_col_name])
             global nom
             nom = ArcGIS()
 
-            coordinates = [test(address) for address in addresses]
-            latitudes = [coordinate.latitude if coordinate is not None else None for coordinate in coordinates ]
-            longitudes = [coordinate.longitude if coordinate is not None else None for coordinate in coordinates ]
-            
+            coordinates = [nom.geocode(address) for address in addresses]
+            latitudes = [
+                coordinate.latitude if coordinate is not None else None for coordinate in coordinates]
+            longitudes = [
+                coordinate.longitude if coordinate is not None else None for coordinate in coordinates]
+
             df["Latitude"] = latitudes
             df["Longitude"] = longitudes
 
-            result = df.to_html(classes="table table-bordered table-hover w-auto", index=False)
+            result = df.to_html(
+                classes="table table-bordered table-hover w-auto", index=False)
             return render_template("index.html", table=result, btn="download_btn.html")
-        
+
         except UnicodeDecodeError:
             return render_template("index.html", text="Please upload a CSV file.", classType="text-danger")
         except pd.errors.EmptyDataError:
             return render_template("index.html", text="File empty", classType="text-danger")
     else:
         return redirect("/")
-    
-def test(address):
-    coded = nom.geocode(address)
-    print(coded)
-    return coded
 
 
 @app.route("/download")
-def download(): 
-    global df  
+def download():
+    global df
     response = make_response(df.to_csv(index=False))
     response.headers["Content-Disposition"] = "attachment; filename=export.csv"
     response.headers["Content-Type"] = "text/csv"
     return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
